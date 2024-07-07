7c06c72ee47a29b115b4281129f7218bd2a66068
Suganya <ymsuganya@gmail.com>
2024-07-07 12:35:34 +0530

    This Repo contains the source code for the
    Exercise that we have done in the ATB7x batch
    
     - Core JAVA
    
    - OOPs
    
    - Collections Framework
    
    - Multi Threading
    
    - Design Patterns
    
    - FILE IO
    
    - Other misc concepts
    
    - Exception

---
+++ src/sample_Add.java
@@ -0,0 +1,10 @@
+public class sample_Add
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=4;
+        int c=a+b;
+        System.out.println("Value of C " +c );
+    }
+}
---
+++ src/sample_Ex_07.java
@@ -0,0 +1,7 @@
+public class sample_Ex_07 {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
+
